411e2c869aeed49334b62ad66b4c69def2454ad6
Karthik <karthik@Karthiks-MacBook-Pro-2.local>
2020-06-13 12:43:52 -0400

    Post Create/ Solved cors

--- src/components/PostCreate.js
+++ src/components/PostCreate.js
@@ -3,12 +3,15 @@ import axios from 'axios'
 
 //functional component
 export default () => {
+    const [title, setTitle] = useState('')
 
-    const onSubmit = () => {
-        axios.post('/posts/',{
-            
-        })
-    }
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await axios.post('http://localhost:4000/posts',{
+            title
+        });
+        setTitle('');
+    };
     
     return(
         <div>
@@ -16,7 +19,7 @@ export default () => {
                 <h1>Create Post</h1>
                 <div className="form-group">
                 <label> Title </label>
-                <input className="form-control"></input>
+                <input value={title} onChange={e => setTitle(e.target.value)} className="form-control"></input>
                 </div>
                 <button className="btn btn-primary">Submit</button>
             </form>
